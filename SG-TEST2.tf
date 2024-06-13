7a9aba999cacb21822c099f9b21ef71290ccad41
CholletiRohith <36255681+CholletiRohith@users.noreply.github.com>
2024-06-13 15:19:43 +0530

    Create storageaccount via API  ./SG-TEST2.tf

---
+++ SG-TEST2.tf
@@ -0,0 +1,10 @@
+module "storageaccount" {
+  source  = "app.terraform.io/simplifycloud/storageaccount/azurerm"
+  version = "1.1.0"
+  resourcegroup      = "rg-test2"
+  location                 = "east us"
+  storagetier             = "Hot"
+  storage_redunancy = "LRS"
+  app = "API-Workspace"
+  env = "dev"
+}
