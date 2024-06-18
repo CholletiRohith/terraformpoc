4ec89187cf901262fd0579636e04aad8fa9d106f
CholletiRohith <36255681+CholletiRohith@users.noreply.github.com>
2024-06-18 16:48:51 +0530

    Create resoucegroup via API  ./RG-ROHITHCLEARINTERVAL4CHATGPT.tf

---
+++ RG-ROHITHCLEARINTERVAL4CHATGPT.tf
@@ -0,0 +1,6 @@
+module "resourcegroup-rohithclearinterval4chatgpt" {
+  source  = "app.terraform.io/simplifycloud/resourcegroup/azurerm"
+  version = "1.1.0"
+  rglocation = "east us"
+  rgname = "rohithclearinterval4chatgpt"
+}
