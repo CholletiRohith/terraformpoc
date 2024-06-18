14ce04618b8e9cb0bd02f8847eb22b732094e553
CholletiRohith <36255681+CholletiRohith@users.noreply.github.com>
2024-06-18 19:45:18 +0530

    Delete SG-DEFR.tf

--- SG-DEFR.tf
+++
@@ -1,9 +0,0 @@
-module "storageaccount-defr" {
-  source  = "app.terraform.io/simplifycloud/storageaccount/azurerm"
-  version = "1.2.0"
-  storagename = "defr"
-  resourcegroup = "rg-test2"
-  location                 = "east us"
-  storagetier             = "Standard"
-  storage_redunancy = "LRS"
-}
