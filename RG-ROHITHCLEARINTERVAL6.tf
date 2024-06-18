927f76091b608bfa1d8b445544ea787bd2d624ac
CholletiRohith <36255681+CholletiRohith@users.noreply.github.com>
2024-06-18 16:54:50 +0530

    Create resoucegroup via API  ./RG-ROHITHCLEARINTERVAL6.tf

---
+++ RG-ROHITHCLEARINTERVAL6.tf
@@ -0,0 +1,6 @@
+module "resourcegroup-rohithclearinterval6" {
+  source  = "app.terraform.io/simplifycloud/resourcegroup/azurerm"
+  version = "1.1.0"
+  rglocation = "east us"
+  rgname = "rohithclearinterval6"
+}
