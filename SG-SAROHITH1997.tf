7b7f3d71c2adc494ab739b9b25122b355a3568a4
CholletiRohith <36255681+CholletiRohith@users.noreply.github.com>
2024-06-18 14:44:32 +0530

    Delete SG-SAROHITH1997.tf

--- SG-SAROHITH1997.tf
+++
@@ -1,9 +0,0 @@
-module "storageaccount" {
-  source  = "app.terraform.io/simplifycloud/storageaccount/azurerm"
-  version = "1.2.0"
-  storagename = "sarohith1997"
-  resourcegroup = "test2"
-  location                 = "east us"
-  storagetier             = "Standard"
-  storage_redunancy = "LRS"
-}
