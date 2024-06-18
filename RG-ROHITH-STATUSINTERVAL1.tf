5efd2188ec658fe49b146650b7fb722c6fc6bb4b
CholletiRohith <36255681+CholletiRohith@users.noreply.github.com>
2024-06-18 19:16:38 +0530

    Delete RG-ROHITH-STATUSINTERVAL1.tf

--- RG-ROHITH-STATUSINTERVAL1.tf
+++
@@ -1,6 +0,0 @@
-module "resourcegroup-rohith-statusinterval1" {
-  source  = "app.terraform.io/simplifycloud/resourcegroup/azurerm"
-  version = "1.1.0"
-  rglocation = "east us"
-  rgname = "rohith-statusinterval1"
-}
