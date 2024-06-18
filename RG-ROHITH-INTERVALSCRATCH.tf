807fe30a5680727e34658301c39101886949f079
CholletiRohith <36255681+CholletiRohith@users.noreply.github.com>
2024-06-18 16:29:46 +0530

    Create resoucegroup via API  ./RG-ROHITH-INTERVALSCRATCH.tf

---
+++ RG-ROHITH-INTERVALSCRATCH.tf
@@ -0,0 +1,6 @@
+module "resourcegroup-rohith-intervalscratch" {
+  source  = "app.terraform.io/simplifycloud/resourcegroup/azurerm"
+  version = "1.1.0"
+  rglocation = "east us"
+  rgname = "rohith-intervalscratch"
+}
