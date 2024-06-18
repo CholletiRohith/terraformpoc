0bbb22f55e6eaeb61eae7dbc0c314c6bf0e97c35
CholletiRohith <36255681+CholletiRohith@users.noreply.github.com>
2024-06-18 19:43:33 +0530

    Delete RG-ROHITHCLEARINTERVAL7.tf

--- RG-ROHITHCLEARINTERVAL7.tf
+++
@@ -1,6 +0,0 @@
-module "resourcegroup-rohithclearinterval7" {
-  source  = "app.terraform.io/simplifycloud/resourcegroup/azurerm"
-  version = "1.1.0"
-  rglocation = "east us"
-  rgname = "rohithclearinterval7"
-}
