b3faa4b8a7ee9cab04f59ebd2226d8cfbb3b8c21
CholletiRohith <36255681+CholletiRohith@users.noreply.github.com>
2024-06-18 15:24:15 +0530

    Create resoucegroup via API  ./RG-ROHITH-STATUSINTERVAL1.tf

---
+++ RG-ROHITH-STATUSINTERVAL1.tf
@@ -0,0 +1,6 @@
+module "resourcegroup-rohith-statusinterval1" {
+  source  = "app.terraform.io/simplifycloud/resourcegroup/azurerm"
+  version = "1.1.0"
+  rglocation = "east us"
+  rgname = "rohith-statusinterval1"
+}
