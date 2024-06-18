63d382416e86e595bfa3e7c2194190392355c87a
CholletiRohith <36255681+CholletiRohith@users.noreply.github.com>
2024-06-18 19:45:29 +0530

    Delete SG-SARFF34.tf

--- SG-SARFF34.tf
+++
@@ -1,9 +0,0 @@
-module "storageaccount-sarff34" {
-  source  = "app.terraform.io/simplifycloud/storageaccount/azurerm"
-  version = "1.2.0"
-  storagename = "sarff34"
-  resourcegroup = "rg-test2"
-  location                 = "east us"
-  storagetier             = "Standard"
-  storage_redunancy = "LRS"
-}
