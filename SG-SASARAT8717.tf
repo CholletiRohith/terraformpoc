b906cb449f33f5862f2d84a50d0c583d22b513cf
CholletiRohith <36255681+CholletiRohith@users.noreply.github.com>
2024-06-18 15:13:30 +0530

    Delete SG-SASARAT8717.tf

--- SG-SASARAT8717.tf
+++
@@ -1,9 +0,0 @@
-module "storageaccount" {
-  source  = "app.terraform.io/simplifycloud/storageaccount/azurerm"
-  version = "1.2.0"
-  storagename = "sasarat8717"
-  resourcegroup = "rg-test2"
-  location                 = "east us"
-  storagetier             = "Standard"
-  storage_redunancy = "LRS"
-}
