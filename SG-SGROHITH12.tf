be9e47531f015ff2f8d5e4a21262ac9ac6126ca2
CholletiRohith <36255681+CholletiRohith@users.noreply.github.com>
2024-06-18 20:36:47 +0530

    Delete SG-SGROHITH12.tf

--- SG-SGROHITH12.tf
+++
@@ -1,9 +0,0 @@
-module "storageaccount-sgrohith12" {
-  source  = "app.terraform.io/simplifycloud/storageaccount/azurerm"
-  version = "1.2.0"
-  storagename = "sgrohith12"
-  resourcegroup = "rg-test2"
-  location                 = "east us"
-  storagetier             = "Standard"
-  storage_redunancy = "LRS"
-}
