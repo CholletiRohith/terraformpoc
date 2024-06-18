83e0fe4fe8b9d26b106471166f41f0f6a9879214
CholletiRohith <36255681+CholletiRohith@users.noreply.github.com>
2024-06-18 19:45:41 +0530

    Delete SG-SARODN.tf

--- SG-SARODN.tf
+++
@@ -1,9 +0,0 @@
-module "storageaccount-sarodn" {
-  source  = "app.terraform.io/simplifycloud/storageaccount/azurerm"
-  version = "1.2.0"
-  storagename = "sarodn"
-  resourcegroup = "rg-test2"
-  location                 = "east us"
-  storagetier             = "Standard"
-  storage_redunancy = "LRS"
-}
