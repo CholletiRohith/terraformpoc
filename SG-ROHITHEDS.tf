374e63aa13c05c6962cee8c057706f79985a233d
CholletiRohith <36255681+CholletiRohith@users.noreply.github.com>
2024-06-18 20:37:39 +0530

    Delete SG-ROHITHEDS.tf

--- SG-ROHITHEDS.tf
+++
@@ -1,9 +0,0 @@
-module "storageaccount-rohitheds" {
-  source  = "app.terraform.io/simplifycloud/storageaccount/azurerm"
-  version = "1.2.0"
-  storagename = "rohitheds"
-  resourcegroup = "rg-test2"
-  location                 = "east us"
-  storagetier             = "Standard"
-  storage_redunancy = "LRS"
-}
