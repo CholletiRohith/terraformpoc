f2202dff0f0be636cf1c2289f0a3eed1ef54d1ea
CholletiRohith <36255681+CholletiRohith@users.noreply.github.com>
2024-06-18 14:28:50 +0530

    Delete SG-TEST2.tf

--- SG-TEST2.tf
+++
@@ -1,8 +0,0 @@
-module "storageaccount" {
-  source  = "app.terraform.io/simplifycloud/storageaccount/azurerm"
-  version = "1.1.0"
-  location                 = "east us"
-  storagetier             = "Hot"
-  storage_redunancy = "LRS"
-  app = "API-Workspace"
-}
