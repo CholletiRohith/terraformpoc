2c443be5d65c7f5f4ea3b6f36d8d7bfa6254847f
CholletiRohith <36255681+CholletiRohith@users.noreply.github.com>
2024-06-18 16:08:34 +0530

    Create resoucegroup via API  ./RG-ROHITHTESTINTERVAL3.tf

---
+++ RG-ROHITHTESTINTERVAL3.tf
@@ -0,0 +1,6 @@
+module "resourcegroup-rohithtestinterval3" {
+  source  = "app.terraform.io/simplifycloud/resourcegroup/azurerm"
+  version = "1.1.0"
+  rglocation = "east us"
+  rgname = "rohithtestinterval3"
+}
