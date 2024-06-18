f754f377edce31f64689a07a965606252f428b30
CholletiRohith <36255681+CholletiRohith@users.noreply.github.com>
2024-06-18 19:44:43 +0530

    Delete RG-ROHITHTESTINTERVAL3.tf

--- RG-ROHITHTESTINTERVAL3.tf
+++
@@ -1,6 +0,0 @@
-module "resourcegroup-rohithtestinterval3" {
-  source  = "app.terraform.io/simplifycloud/resourcegroup/azurerm"
-  version = "1.1.0"
-  rglocation = "east us"
-  rgname = "rohithtestinterval3"
-}
