c6c729e5040951927af93609d6ea743b09fb5fad
CholletiRohith <36255681+CholletiRohith@users.noreply.github.com>
2024-06-18 20:27:57 +0530

    Create storageaccount via API  ./SG-ROHITHDE.tf

---
+++ SG-ROHITHDE.tf
@@ -0,0 +1,9 @@
+module "storageaccount-rohithde" {
+  source  = "app.terraform.io/simplifycloud/storageaccount/azurerm"
+  version = "1.2.0"
+  storagename = "rohithde"
+  resourcegroup = "rg-test2"
+  location                 = "east us"
+  storagetier             = "Standard"
+  storage_redunancy = "LRS"
+}
