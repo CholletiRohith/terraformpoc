77f0052272702c8eddcd350054d4ea7400936797
CholletiRohith <36255681+CholletiRohith@users.noreply.github.com>
2024-07-03 14:48:10 +0530

    Delete RG-TEST-03-07-2024-2.tf

--- RG-TEST-03-07-2024-2.tf
+++
@@ -1,6 +0,0 @@
-module "resourcegroup-test-03-07-2024-2" {
-  source  = "app.terraform.io/simplifycloud/resourcegroup/azurerm"
-  version = "1.1.0"
-  rglocation = "east us"
-  rgname = "test-03-07-2024-2"
-}
